15e421f79290a13b2870b20998e30a028535e07b
attreid <attreid@gmail.com>
2017-03-24 15:11:59 +0100

    pridani ipInRange

--- src/Strings.php
+++ src/Strings.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace NAttreid\Utils;
 
@@ -83,5 +83,22 @@ class Strings extends \Nette\Utils\Strings
 		return false;
 	}
 
-
+	/**
+	 * @param  string $ip IP to check in IPV4 format eg. 127.0.0.1
+	 * @param  string $range IP/CIDR netmask eg. 127.0.0.0/24, also 127.0.0.1 is accepted and /32 assumed
+	 * @return bool
+	 */
+	function ipInRange(string $ip, string $range): bool
+	{
+		if (strpos($range, '/') == false) {
+			$range .= '/32';
+		}
+		// $range is in IP/CIDR format eg 127.0.0.1/24
+		list($range, $netmask) = explode('/', $range, 2);
+		$range_decimal = ip2long($range);
+		$ip_decimal = ip2long($ip);
+		$wildcard_decimal = pow(2, (32 - $netmask)) - 1;
+		$netmask_decimal = ~$wildcard_decimal;
+		return (($ip_decimal & $netmask_decimal) == ($range_decimal & $netmask_decimal));
+	}
 }
